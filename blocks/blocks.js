e19bad405af6b1392dbe53ba832356cdfba599de
APHIBUN <63030101@KMITL.AC.TH>
2022-06-22 15:54:26 +0700

    aphibun

---
+++ blocks/blocks.js
@@ -0,0 +1,10 @@
+Blockly.defineBlocksWithJsonArray([
+{
+  "type": "max90614_object_temp",
+  "message0": "object temperature (°C)",
+  "output": "Number",
+  "colour": "#27AE60",
+  "tooltip": "Read object temperature from MLX90614",
+  "helpUrl": ""
+}
+]);
---
+++ blocks/generators.js
@@ -0,0 +1,6 @@
+Blockly.Python['max90614_object_temp'] = function (block) {
+  Blockly.Python.definitions_['import_MLX90614'] = 'import MLX90614';
+
+  var code = 'MLX90614.readObjectTempC()';
+  return [code, Blockly.Python.ORDER_NONE];
+};
