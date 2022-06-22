db3cd22c7104d6ac103a67349c16d8b9860d1d54
APHIBUN <92081596+Aphibun@users.noreply.github.com>
2022-06-22 16:02:50 +0700

    1

--- extension.js
+++ extension.js
@@ -1,13 +1,12 @@
 ({
-    name: "ZX-THERMO", // Category Name
-    description: "Non-Contact Temperature Sensor",
-    author: "INEX",
+    name: "DISTANCE", // Category Name
+    description: "distance Sensor",
+    author: "Aphibun",
     category: "Sensors",
     version: "1.0.0",
     icon: "/static/icon.png", // Category icon
     color: "#27AE60", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
-        "max90614_object_temp",
-        "max90614_ambient_temp",
+        "max90614_object_temp"
     ]
 });
